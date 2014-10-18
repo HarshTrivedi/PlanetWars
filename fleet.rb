2c8c831f1ad217fae2197d9e242e3b5a975e34d8
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-10-18 08:30:45 +0530

    integrate Fleet class with FleetShape class and add required utility methods to it

--- fleet.rb
+++ fleet.rb
@@ -1,22 +1,45 @@
 class Fleet
+
+	@app
 	attr_accessor :shape ,:owner , :num_ships , :source_planet , :destination_planet , :total_length , :length_left
+
 	def initialize(owner , source_planet , destination_planet , num_ships)
+
 		@owner = owner
 		@num_ships = num_ships
 		@source_planet = source_planet
 		@destination_planet = destination_planet
-		@total_length = Math::sqrt((source_planet.shape.x - destination_planet.shape.x)**2 + (source_planet.shape.y - destination_planet.shape.y)**2 ) 
+		@total_length = Math::sqrt((source_planet.shape.left - destination_planet.shape.left)**2 + (source_planet.shape.top - destination_planet.shape.top)**2 ) 
 
-
+		left = @source_planet.left + @source_planet.radius
+		top = @source_planet.top + @source_planet.radius
+		radius = 10 # fixed for fleet
+		# color is F of owner (owner.color)
+		color = green
+		@shape = FleetShape.new( @app , left , top , radius , num_ships, color)
+		@shape.move(@destination_planet.left , @destination_planet.top)
 	end
 
-	def fly
-		@shape = oval(top:  , left:  , radius: 10)
-		##Do all the animation here# animate the 'shape'
-	end
+	# def fly()
+	# 	@shape.move()
+	# end
 
 	def length_left
-		Math::sqrt((shape.x - destination_planet.shape.x)**2 + (shape.y - destination_planet.shape.y)**2 ) 
+		Math::sqrt((left - @destination_planet.left)**2 + (top - @destination_planet.top)**2 ) 
 	end
+
+
+	def left
+		@shape.left
+	end
+
+	def top
+		@shape.top
+	end
+
+	def centre
+		@shape.left + @shape.radius
+	end
+
 
 end
